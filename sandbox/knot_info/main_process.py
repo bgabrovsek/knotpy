c64b8f81720810255706fb6abad6bb7968c565d6
bgabrovsek <bostjan.gabrovsek@gmail.com>
2024-04-08 12:07:57 +0200

    Remove the now ignored directory "sandbox"

--- sandbox/knot_info/main_process.py
+++
@@ -1,9 +0,0 @@
-
-import pandas
-
-
-df = pandas.read_excel("knotinfo_data_complete.xls")
-
-extract = ["name", "pd_notation", "jones_polynomial"]
-
-df[extract][1:].to_csv("name-pd-jones.csv", index=False, header=True)
