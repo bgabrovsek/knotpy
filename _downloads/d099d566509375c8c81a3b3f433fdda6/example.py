b6d12ea744b12cbf5cd60867d2e992fa50502a9c
drobilc <drobilc@users.noreply.github.com>
2024-04-11 12:04:04 +0000

    deploy: b0808463cc2e7e72ccf58f249746fc63aed4e324

---
+++ _downloads/d099d566509375c8c81a3b3f433fdda6/example.py
@@ -0,0 +1,13 @@
+"""
+=====================
+Spatial graph example
+=====================
+
+A short description of what the example does.
+"""
+
+import knotpy as kp
+from knotpy.generate.example import example_spatial_graph
+
+g = example_spatial_graph()
+print(g)
