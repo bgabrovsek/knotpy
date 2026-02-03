c379112b3a2699bbd603e66edd7ff6b3045a3145
bgabrovsek <bgabrovsek@users.noreply.github.com>
2026-02-03 13:56:18 +0000

    deploy: 191491eb2fddbf5ca3cdbabb1f0832a31d52c485

--- _downloads/671fda9c191a1ea1dae21aa369248dbc/02-plot.py
+++ _downloads/671fda9c191a1ea1dae21aa369248dbc/02-plot.py
@@ -3,10 +3,13 @@
 Knot example
 =====================
 
-Draw the trefoil knot.
+Draw the diagram of the trefoil knot (oriented/non-oriented)
 """
 
 import knotpy as kp
+
+k = kp.knot("3_1")
+kp.draw(k, show=True)
 
 k = kp.knot("+3_1")
 kp.draw(k, show=True)
